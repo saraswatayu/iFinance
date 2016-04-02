fc5b57a16d11a8d1994e74988f6092bb46edca7a
Ayush Saraswat <saraswatayu@gmail.com>
2016-04-01 22:34:37 -0700

    updates to CSV

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateAccountsTable' => $baseDir . '/database/migrations/2015_10_12_010000_create_accounts_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateTransactionsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_transactions_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
